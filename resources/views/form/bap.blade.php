6bb5d59d14bbdfc1c08680caf6a06e8c5d9d093f
nem95 <naiim.chatti@gmail.com>
2016-03-14 16:30:36 +0100

    formulaire BAP complet, suite a faire , la migration de la table et le controller pour le formulaire

--- resources/views/form/bap.blade.php
+++ resources/views/form/bap.blade.php
@@ -4,9 +4,10 @@
     {{csrf_field()}}
     {{-- Champ nom de la BAP--}}
 
-    <div class="form-group col-md-6 col-xs-6">
+    <div class="form-group col-md-12 ">
         {!! Form::label('titre', 'Nom du projet') !!}
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse',
+        'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ nom client--}}
@@ -18,138 +19,167 @@
     </div>
 
     {{-- Champ prenom--}}
-    {!! Form::label('name_du_champs', ' Prenom ') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::label('name_du_champs', ' Prenom ') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ fonction--}}
-    {!! Form::label('name_du_champs', 'fonction du commanditaire du projet') !!}
-    <div class="form-group">
+    <div class="form-group col-md-12">
+        {!! Form::label('name_du_champs', 'fonction du commanditaire du projet') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ adresse--}}
-    {!! Form::label('name_du_champs', 'Adresse postale') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::label('name_du_champs', 'Adresse postale') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ e-mail--}}
-    {!! Form::label('name_du_champs', 'Email') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::label('name_du_champs', 'Email') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ telephone--}}
-    {!! Form::label('name_du_champs', 'Téléphone') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::label('name_du_champs', 'Telephone') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ infos du contact ( nom prenom adresse email et telephone)--}}
-    {!! Form::label('name_du_champs', 'Nom et fonction du contact pour le suivi du projet avec étudiants ') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-12 ">
+        {!! Form::label('name_du_champs', 'Nom et fonction du contact pour le suivi du projet avec etudiants ') !!}<br>
+        {!! Form::label('name_du_champs', 'Nom & Prenom') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ adresse contact etudiant--}}
-    {!! Form::label('name_du_champs', 'Adresse postale') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::label('name_du_champs', 'Adresse postale') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ e-mail contact etudiant--}}
-    {!! Form::label('name_du_champs', 'Email') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::label('name_du_champs', 'Email') !!}
         {!! Form::email('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ telephone contact etudiant--}}
-    {!! Form::label('name_du_champs', 'Téléphone') !!}
-    <div class="form-group">
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::label('name_du_champs', 'Telephone') !!}
         {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+    </div>
+    <div class="row">
     </div>
 
     {{-- Champ de description du projet --}}
+
+
+    <div class="form-group col-md-12">
+        {!! Form::label('name_du_champs', 'A propos de vous') !!}
+        {!! Form::textarea('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+    </div>
+    <div class="col-md-12"><h2>DESCRIPTION DU PROJET</h2><br></div>
     {{-- Champ site internet--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'Site Internet  ') !!}
     </div>
     {{-- Champ 3d--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', '3D') !!}
     </div>
     {{-- Champ animation 2d--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'Animation 2D') !!}
     </div>
     {{-- Champ installation multimedia--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'Installation Multimedia') !!}
     </div>
     {{-- Champ jeu video--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'Jeu video') !!}
     </div>
     {{-- Champ DVD--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'DVD') !!}
     </div>
     {{-- Champ print--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'Pint') !!}
     </div>
     {{-- Champ CD-ROM --}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'CD-ROM') !!}
     </div>
     {{-- Champ evenement--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'Evenement') !!}
     </div>
     {{-- Champ autre--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-6 col-xs-6">
+        {!! Form::checkbox('name', 'value', false)!!}
+        {!! Form::label('name_du_champs', 'Autre ...') !!}
     </div>
 
     {{-- Champ La demande client avec un textarea si possible --}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-12">
+        {!! Form::label('name_du_champs', 'CONTEXTE DE LA DEMANDE') !!}
+        {!! Form::textarea('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ contexte de la demande--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-12">
+        {!! Form::label('name_du_champs', 'VOTRE DEMANDE') !!}
+        {!! Form::textarea('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ vos objectifs--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
+
+    <div class="form-group col-md-12">
+        {!! Form::label('name_du_champs', 'VOS OBJECTIFS') !!}
+        {!! Form::textarea('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
 
     {{-- Champ contraintes particulière et infos complementaires--}}
-    {!! Form::label('name_du_champs', 'titre') !!}
-    <div class="form-group">
-        {!! Form::text('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
-    </div>
 
-    <div class="form-group">
-        {!! Form::textarea('description', null ,['class'=>'form-control', 'maClasse', 'placeholder'=>'', 'Mon champs']) !!}
+    <div class="form-group col-md-12">
+        {!! Form::label('name_du_champs', 'CONTRAINTES PARTICULIERES EVENTUELLES
+ET INFORMATIONS COMPLEMENTAIRES') !!}
+        {!! Form::textarea('title', null, ['class'=> 'form-control', 'maClasse', 'placeholder'=> '', 'Mon champs']) !!}
     </div>
+    
     {!! Form::submit('Envoyer', ['class'=>'btn btn-block']) !!}
 
     {!! Form::close() !!}
